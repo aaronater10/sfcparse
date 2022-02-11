1ccb51bb3cff899691b50a867df5229f6e372860
aaronater10 <dev_admin@dunnts.com>
2022-02-11 17:31:18 -0600

    added xml section copying ini section

--- src/sfcparse.py
+++ src/sfcparse.py
@@ -28,7 +28,8 @@ import yaml as __yaml
 from configparser import SafeConfigParser as __SafeConfigParser
 from configparser import ExtendedInterpolation as __ExtendedInterpolation
 import hashlib as __hashlib
-import xml.etree.ElementTree as __xml_tree
+import xml.etree.ElementTree as __xml_etree
+import xml.dom.minidom as __xml_dom
 
 
 #########################################################################################################
@@ -628,3 +629,91 @@ def comparefilehash(file_to_hash: str, stored_hash_file: str, hash_algorithm: st
     except FileNotFoundError: raise
 
     raise TypeError("BAD ERROR")
+
+
+#########################################################################################################
+# XML: Build xml data. Export, and Import xml files
+def buildxmlauto(data: dict, xml_parse_type: str='etree') -> __SafeConfigParser:
+    """
+    Auto converts python dict to ini data structure.
+
+    Returns a ConfigParser obj with your data. Assign the output to var
+
+    Enter correctly structured python dict to convert to ini.
+
+    [Example Python dict]
+
+    {
+        'section1': python_dict,
+        'section2': python_dict    
+    }
+
+    This is using the native configparser library shipped with the python standard libray. Using SafeConfigParser method.
+    For more information on the configparser library, visit: https://docs.python.org/3/library/configparser.html
+    """
+    __err_msg = f"buildiniauto - Invalid data, type, or nothing specified: {data}"
+    # Auto Build INI data structure
+    __ini_data = __SafeConfigParser(interpolation=__ExtendedInterpolation())
+    for key,value in data.items():
+        __ini_data[key] = value
+    return __ini_data
+
+
+def buildxmlmanual(xml_parse_type: str='etree') -> __SafeConfigParser:
+    """    
+    Returns an empty ConfigParser obj to manually build ini data
+    
+    Assign the output to var
+
+    This is using the native configparser library shipped with the python standard libray. Using SafeConfigParser method.
+    For more information on the configparser library, visit: https://docs.python.org/3/library/configparser.html
+    """
+    return __SafeConfigParser(interpolation=__ExtendedInterpolation())
+
+
+def importxmlfile(filename: str, xml_parse_type: str='etree') -> __SafeConfigParser:
+    """
+    Imports ini data from a file.
+
+    Returns a ConfigParser obj. Assign the output to var
+
+    Enter ini file location as str to import.
+
+    [Example Use]
+
+    importinifile('path/to/filename.ini')
+
+    This is using the native configparser library shipped with the python standard libray. Using SafeConfigParser method.
+    For more information on the configparser library, visit: https://docs.python.org/3/library/configparser.html
+    """
+    __parser = __SafeConfigParser(interpolation=__ExtendedInterpolation())
+    __parser.read(filename)
+    return __parser
+
+
+# Create hollow reference name for "ini_data" to denote ini data needs to be exported for hinting exports
+class __dummy_xml:
+    """Not meant to be used"""
+    class xml_data: pass
+    """Not meant to be used"""
+
+def exportxmlfile(filename: str, data: __dummy_xml.xml_data, xml_parse_type: str='etree') -> None:
+    """
+    Exports a new file from a ini data (ConfigParser) obj
+    
+    Enter new filename as str. Pass ini data for output to file
+    
+    [Example Use]
+
+    exportinifile('path/to/filename.ini', data)
+
+    This is using the native configparser library shipped with the python standard libray. Using SafeConfigParser method.
+    For more information on the configparser library, visit: https://docs.python.org/3/library/configparser.html
+    """
+    __err_msg = f"exportinifile - Invalid data to export, type, or nothing specified: {filename}"
+    if type(data) == type(__SafeConfigParser()):
+        with open(filename, 'w') as f:
+            data.write(f)
+        return None
+    
+    raise TypeError(__err_msg)
