8343db17dba316878f9bdc629d89c085e410a5c1
aaronater10 <dev_admin@dunnts.com>
2022-02-18 17:41:16 -0600

    added writing byte data functionality in exportfile

--- src/sfcparse.py
+++ src/sfcparse.py
@@ -153,20 +153,38 @@ def importfile(filename: str) -> __dummy_class.Class:
 
 #########################################################################################################
 # Export Data to File
-def exportfile(filename: str, *data: __Any):
+def exportfile(filename: str, *data: __Any, byte_data: bool=False):
     """
     Exports a new file with the new data.
     
     Enter new filename as str, Pass any data type for output to file.
+
+    [Options]
+    byte_data: Set to True if converting byte data to it's actual value to file
     
     [Example Use]
-    exportfile('filename.test' or 'path/to/filename.test', 'data1', 'data2')
+    Normal: exportfile('path/of/filename', 'data')
+
+    Byte Data: exportfile('path/of/filename', b'data', byte_data=True)
     """
+    __err_msg_bytes = f"exportfile - Only bytes is allowed for data: {data}"
 
     # Export data to new file
-    with open(filename, 'w') as f:
+
+    # Raw Data to File
+    if not byte_data:
+        with open(filename, 'w') as f:
+            for data_to_write in data:
+                f.writelines(str(data_to_write))
+    # Byte Data Converted to File
+    if byte_data:
         for data_to_write in data:
-            f.writelines(str(data_to_write))
+            if not isinstance(data_to_write, bytes):
+                raise TypeError(__err_msg_bytes)
+
+        with open(filename, 'wb') as f:
+            for data_to_write in data:
+                f.write(data_to_write)
 
 
 #########################################################################################################
