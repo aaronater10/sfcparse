a002156d5a880f2fcd51b9cf166e55cb05237ef9
aaronater10 <dev_admin@dunnts.com>
2022-02-19 16:18:01 -0600

    added ini str interpolation test

--- tests/ini/test_iniimportfile.py
+++ tests/ini/test_iniimportfile.py
@@ -31,3 +31,22 @@ def test1_ini_file_import():
     for section,_ in ini_data.items():
         assert str(ini_data[section]['key']) == file_import.get(section, 'key')
         assert isinstance(file_import.get(section, 'key'), str)
+
+
+# 2. INI ExtendedInterpolation Import - Importing ini file data and test str interpolation and attributes
+def test2_ini_extended_interpolation():
+    filename = '2_import_interpolate_data.ini'
+    filepath = test_file_path + filename
+    str_interpolate_match = 'data-256-2.5-True'
+
+    # Test imported data and it's Type
+    file_import = sfcparse.iniimportfile(filepath)
+
+    sections_match = ['str_data', 'int_data', 'float_data', 'bool_data', 'str_interpolate']
+    assert (file_import.sections() == sections_match) and (isinstance(file_import.sections(), list))
+
+    assert (file_import.get('str_interpolate', 'key') == str_interpolate_match) and (isinstance(file_import.get('str_interpolate', 'key'), str))
+    assert (file_import.get('str_data', 'key') == 'data') and (isinstance(file_import.get('str_data', 'key'), str))
+    assert (file_import.get('int_data', 'key') == '256') and (isinstance(file_import.get('int_data', 'key'), str))
+    assert (file_import.get('float_data', 'key') == '2.5') and (isinstance(file_import.get('float_data', 'key'), str))
+    assert (file_import.get('bool_data', 'key') == 'True') and (isinstance(file_import.get('bool_data', 'key'), str))
