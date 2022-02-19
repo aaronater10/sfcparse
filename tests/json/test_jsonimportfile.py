e0d20c0d433d019af07b2c1a3613903a64a72e6d
aaronater10 <dev_admin@dunnts.com>
2022-02-19 01:32:27 -0600

    add json import file tests

---
+++ tests/json/test_jsonimportfile.py
@@ -0,0 +1,23 @@
+# jsonimportfile - Tests
+from src import sfcparse
+
+test_file_path = './tests/test_files/json/'
+
+
+################################################################
+# TESTS
+
+
+# 1. JSON Data Import - Importing json file data and test attributes
+def test1_data_bytes_import():
+    filename = '1_import_data.json'
+    filepath = test_file_path + filename    
+
+    # Test Data and it's Type
+    json_import = sfcparse.jsonimportfile(filepath)
+
+    assert (json_import['string_data'] == 'data') and (isinstance(json_import['string_data'], str))
+    assert (json_import['int_data'] == 256) and (isinstance(json_import['int_data'], int))
+    assert (json_import['array_data'] == [1,2,3]) and (isinstance(json_import['array_data'], list))
+    assert (json_import['bool_data'] == True) and (isinstance(json_import['bool_data'], bool))
+    assert (json_import['null_data'] == None) and (isinstance(json_import['null_data'], type(None)))
