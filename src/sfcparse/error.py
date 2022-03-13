af46ff7f42072388470ccd362e3a688d82952b80
aaronater10 <dev_admin@dunnts.com>
2022-03-12 18:13:33 -0600

    updated custom exceptions for native sfcparse functions

--- src/sfcparse/error.py
+++ src/sfcparse/error.py
@@ -1,2 +1,15 @@
-# errors - Contains base exception
-class SfcparseError(Exception): pass
+# error - Contains base exception
+class SfcparseError(Exception):
+    """
+    sfcparse base exception
+    """
+    __PARENT_EXCEPTION_NAME = 'sfcparse'
+
+    def __init__(self, msg: str) -> None:
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return self.msg
+    
+    def set_module_name(module_name: str=__PARENT_EXCEPTION_NAME):
+        return module_name
