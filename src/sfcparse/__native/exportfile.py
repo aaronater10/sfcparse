e7f1adbb870eaeca90d94cba8ecf638738a66bd8
aaronater10 <dev_admin@dunnts.com>
2022-03-26 23:45:36 -0500

    updated custom exceptions for exportfile sfcparse functions

--- src/sfcparse/__native/exportfile.py
+++ src/sfcparse/__native/exportfile.py
@@ -25,21 +25,29 @@ def exportfile(filename: str, *data: __Any, byte_data: bool=False):
 
     Byte Data: exportfile('path/of/filename', b'data', byte_data=True)
     """
-    __err_msg_bytes = f"Only bytes is allowed if using byte_data=True: {data}"
+    # Error Checks
+    __err_msg_bytes = f"Only bytes is allowed if using byte_data=True"
+    __err_msg_type_bytes = "Only bool is allowed for byte_data"
+    __err_msg_type_str = "Only str is allowed for filename"
+
+    if not isinstance(byte_data, bool): raise _Exportfile.exportfile(__err_msg_type_bytes, f'\nDATA: {byte_data}')
+    if not isinstance(filename, str): raise _Exportfile.exportfile(__err_msg_type_str, f'\nFILE:"{filename}"')
 
     # Export data to new file
 
     # Raw Data to File
     if not byte_data:
-        with open(filename, 'w') as f:
-            for data_to_write in data:
-                f.writelines(str(data_to_write))
+        try:
+            with open(filename, 'w') as f:
+                for data_to_write in data:
+                    f.writelines(str(data_to_write))
+        except FileNotFoundError as __err_msg: raise _Exportfile.exportfile(__err_msg, f'\nFILE:"{filename}"')
     # Byte Data Converted to File
     if byte_data:
         for data_to_write in data:
-            if not isinstance(data_to_write, bytes):
-                raise _Exportfile.exportfile(__err_msg_bytes, f'"{data}"')
-
-        with open(filename, 'wb') as f:
-            for data_to_write in data:
-                f.write(data_to_write)
+            if not isinstance(data_to_write, bytes): raise _Exportfile.exportfile(__err_msg_bytes, f'\nDATA: {data}')
+        try:
+            with open(filename, 'wb') as f:
+                for data_to_write in data:
+                    f.write(data_to_write)
+        except FileNotFoundError as __err_msg: raise _Exportfile.exportfile(__err_msg, f'\nFILE:"{filename}"')
