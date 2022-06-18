3b0435ff2dc665a365db839efcb15fa100699e17
aaronater10 <dev_admin@dunnts.com>
2022-06-18 15:19:54 -0500

    updated jsonexportstr doc string

--- src/sfcparse/__json/jsonexportstr.py
+++ src/sfcparse/__json/jsonexportstr.py
@@ -20,7 +20,7 @@ def jsonexportstr(data: Union[str, int, float, bool, list, dict, tuple, None], i
 
     [Example Use]
 
-    jsonexportstr(dict, [optional] indent_level)
+    jsonexportstr(str | int | float | bool | list | dict | tuple | None, [optional] indent_level)
 
     This is using the native json library shipped with the python standard library. For more
     information on the json library, visit: https://docs.python.org/3/library/json.html
