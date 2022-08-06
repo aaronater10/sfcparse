eca37d1c2090a600e34254f161915e9d3254355f
aaronater10 <dev_admin@dunnts.com>
2022-08-06 17:58:41 -0500

    added tests for new attr reference feature

--- tests/native/test_importfile.py
+++ tests/native/test_importfile.py
@@ -19,7 +19,6 @@ class TestImportFile(unittest.TestCase):
         assert path.getsize(filepath) == 0, f"File Not Empty: {filename}"
         assert (sfcparse.importfile(filepath)) == None, f"Not None {filename}"
 
-
     # 2. Single Line Import - Importing Singles Lines of All Accepted Data Types
     def test2_single_line_import(self):
         filename = '2_single_line.data'
@@ -41,7 +40,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_none == None) and (isinstance(file_import.data_none, type(None)))
         assert (file_import.data_bytes == b'data') and (isinstance(file_import.data_bytes, bytes))
 
-
     # 3. Multi Line Import - Importing Multi Line of All Accepted Data Types
     def test3_multi_line_import(self):
         filename = '3_multi_line.data'
@@ -56,7 +54,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_dict == {'k1':1, 'k2':2, 'k3':3}) and (isinstance(file_import.data_dict, dict))
         assert (file_import.data_tuple == (1,2,3)) and (isinstance(file_import.data_tuple, tuple))
         assert (file_import.data_set == {1,2,3}) and (isinstance(file_import.data_set, set))
-
 
     # 4. Multi-Single Line Import - Importing Multi and Single Lines Together
     def test4_multi_single_line_import(self):
@@ -76,7 +73,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_bytes == b'data') and (isinstance(file_import.data_bytes, bytes))
         assert (file_import.data_set == {1,2,3}) and (isinstance(file_import.data_set, set))
 
-
     # 5. Multi-Single Comments Import - Importing Multi and Single Lines with Comments
     def test5_multi_single_comments_import(self):
         filename = '5_multi-single_comments.data'
@@ -94,7 +90,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_none == None) and (isinstance(file_import.data_none, type(None)))
         assert (file_import.data_bytes == b'data') and (isinstance(file_import.data_bytes, bytes))
         assert (file_import.data_set == {1,2,3}) and (isinstance(file_import.data_set, set))
-
 
     # 6. Nested Data Import - Importing Nested Data
     def test6_nested_data_import(self):
@@ -114,7 +109,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_list[4] == {1, 2, 3}) and (isinstance(file_import.data_list[4], set))
         assert (file_import.data_list[5] == [1, 2, 3]) and (isinstance(file_import.data_list[5], list))
 
-
     # 7. White Space Import - Importing Data with White Space in Between
     def test7_white_space_import(self):
         filename = '7_white_space.data'
@@ -133,7 +127,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_float == 1.0) and (isinstance(file_import.data_float, float))
         assert (file_import.data_set == {1,2,3}) and (isinstance(file_import.data_set, set))
         assert (file_import.data_bool == True) and (isinstance(file_import.data_bool, bool))
-
 
     # 8. All Multi-Single Line Types Import - Importing All Multi-Single Line Types Together
     def test8_all_multi_single_types_import(self):
@@ -163,7 +156,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_none == None) and (isinstance(file_import.data_none, type(None)))
         assert (file_import.data_bytes == b'data') and (isinstance(file_import.data_bytes, bytes))
 
-
     # 9. Big Data Import - Importing 100K+ Values of Data with Single Lines
     def test9_big_data_import(self):
         filename = '9_big_data_with_singles.data'
@@ -181,7 +173,6 @@ class TestImportFile(unittest.TestCase):
         assert (len(file_import.data_multi) == big_data_len) and (isinstance(file_import.data_multi, dict))
         assert (file_import.data_none == None) and (isinstance(file_import.data_none, type(None)))
         assert (file_import.data_bytes == b'data') and (isinstance(file_import.data_bytes, bytes))
-
 
     # 10. Misc Behavior Import - Importing Misc, Odd, or Unique Data Inputs
     def test10_misc_data_import(self):
@@ -206,7 +197,6 @@ class TestImportFile(unittest.TestCase):
         assert (file_import.data_token1 == ['normal value', "var = 'value'", 'normal value']) and (isinstance(file_import.data_token1, list))
         assert (file_import.data_end_token1 == ['normal value', "var = 'value'", 'normal value']) and (isinstance(file_import.data_end_token1, list))
 
-
     # 11. Single-Line Attr Dedup OFF - Turning OFF Attribute Dedup Feature Test
     def test11_single_attr_dedup_off(self):
         filename = '11_attr_dedup_off_single.data'
@@ -218,7 +208,6 @@ class TestImportFile(unittest.TestCase):
         # Test Attributes and Types - Confirm data and it's type was in fact changed inside file
         assert (file_import.data_dict == "changed data") and (isinstance(file_import.data_dict, str))
 
-
     # 12. Multi-Line Attr Dedup OFF - Turning OFF Attribute Dedup Feature Test
     def test12_multi_attr_dedup_off(self):
         filename = '12_attr_dedup_off_multi.data'
@@ -229,7 +218,6 @@ class TestImportFile(unittest.TestCase):
 
         # Test Attributes and Types - Confirm data and it's type was in fact changed inside file
         assert (file_import.data_list == "changed data") and (isinstance(file_import.data_list, str))
-
 
     # 13. Single-Line Attr Lock - Attribute Locked and Cannot Re-Assign
     def test13_single_attr_lock(self):
@@ -272,7 +260,6 @@ class TestImportFile(unittest.TestCase):
         with self.assertRaises(Exception): file_import.data_bytes = change_value
         self.assertEqual(file_import.data_bytes, b'data')
 
-
     # 14. Single-Line Attr Lock - Attribute Locked and Cannot Re-Assign
     def test14_multi_attr_lock(self):
         filename = '14_attr_lock_multi.data'
@@ -302,5 +289,41 @@ class TestImportFile(unittest.TestCase):
         filepath = test_file_path + filename
 
         # Test File Import
-        sfcparse.importfile(filepath)
-       
+        sfcparse.importfile(filepath)    
+
+    # 16. Attr Referencing - Reference Pre-Defined Attr's Value and Confirm Assigned
+    def test16_attr_ref_single_multi(self):
+        filename = '16_attr_ref_single-multi.data'
+        filepath = test_file_path + filename
+
+        # Test File Import
+        file_import = sfcparse.importfile(filepath)
+
+        # Test Attributes and Types
+        self.assertEqual(file_import.data_list_multi, 1)
+        self.assertEqual(file_import.data_float, {'k1':1, 'k2':2, 'k3':3})
+        self.assertEqual(file_import.data_bool, {'k1':1, 'k2':2, 'k3':3})
+        self.assertEqual(file_import.data_set, (1,2,3))
+    
+    # 17. Attr Referencing Lock - Reference Pre-Defined Attr's Value, Confirm Assigned, and Locked
+    def test17_attr_ref_lock_single_multi(self):
+        filename = '17_attr_ref_lock_single-multi.data'
+        filepath = test_file_path + filename
+
+        # Test File Import
+        file_import = sfcparse.importfile(filepath)
+
+        # Test Attributes and Types - Locked Values Check, then Test Re-Assignment
+        change_value = 'changed_value'
+        
+        self.assertEqual(file_import.data_list_multi, 1)
+        with self.assertRaises(Exception): file_import.data_list_multi = change_value
+
+        self.assertEqual(file_import.data_float, {'k1':1, 'k2':2, 'k3':3})
+        with self.assertRaises(Exception): file_import.data_float = change_value
+
+        self.assertEqual(file_import.data_bool, {'k1':1, 'k2':2, 'k3':3})
+        with self.assertRaises(Exception): file_import.data_bool = change_value
+
+        self.assertEqual(file_import.data_set, (1,2,3))
+        with self.assertRaises(Exception): file_import.data_set = change_value
