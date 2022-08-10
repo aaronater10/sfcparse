2e1eff6dcde3fef7b1dde9ec715ab736447768a1
aaronater10 <dev_admin@dunnts.com>
2022-08-09 19:36:32 -0500

    updated hints for savefile

--- src/sfcparse/__native/savefile.py
+++ src/sfcparse/__native/savefile.py
@@ -3,15 +3,16 @@
 # Imports
 from ast import literal_eval as __literal_eval__
 from os import path as __path
-from typing import Any as _Any
+from typing import Union as __Union
 from ..error import SfcparseError
+from .importfile import SfcparseFileData as __SfcparseFileData
 
 # Exception for Module
 class SaveFile(SfcparseError): __module__ = SfcparseError.set_module_name()
 
 #########################################################################################################
 # Save Data to File
-def savefile():
+def savefile(filename: str, data: __Union['__SfcparseFileData', dict]) -> None:
     """
     savefile
     """
